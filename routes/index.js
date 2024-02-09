a39682e993aa95c5cc68b9ae376bb24f42eb7370
CJ-The-Black-Child <congomigue@gmail.com>
2024-02-09 12:38:06 +0300

    Updated index.js to control routing for app, user, and file operations

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { getStatus, getStats } = require('../controllers/AppController');
+const { postNew, getMe } = require('../controllers/UsersController');
+const { getConnect, getDisconnect } = require('../controllers/AuthController');
+const { postUpload, getShow, getIndex, putPublish, putUnpublish, getFile } = require('../controllers/FilesController');
+
+function routingControl(app) {
+  const router = express.Router();
+  app.use('/', router);
+
+  // App Controller
+  router.get('/status', getStatus);
+  router.get('/stats', getStats);
+
+  // User Controller
+  router.post('/users', postNew);
+
+  // Authenticate user
+  router.get('/connect', getConnect);
+  router.get('/disconnect', getDisconnect);
+  router.get('/users/me', getMe);
+
+  // Files Controller
+  router.post('/files', postUpload);
+  router.get('/files/:id', getShow);
+  router.get('/files', getIndex);
+  router.put('/files/:id/publish', putPublish);
+  router.put('/files/:id/unpublish', putUnpublish);
+  router.get('/files/:id/data', getFile);
+}
+
+module.exports = routingControl;
