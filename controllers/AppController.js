86dd12284b0cf3100074ab4b9677220cb87aa5cc
CJ-The-Black-Child <congomigue@gmail.com>
2024-02-10 07:56:24 +0300

    Updated AppController.js to handle app status and stats

---
+++ controllers/AppController.js
@@ -0,0 +1,14 @@
+const { isAlive } = require('../utils/redis');
+const { isAlive: dbIsAlive, nbUsers, nbFiles } = require('../utils/db');
+
+class AppController {
+  static getStatus(req, res) {
+    res.status(200).send({ redis: isAlive(), db: dbIsAlive() });
+  }
+
+  static async getStats(req, res) {
+    res.status(200).send({ users: await nbUsers(), files: await nbFiles() });
+  }
+}
+
+module.exports = AppController;
