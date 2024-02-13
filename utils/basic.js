58695a0b64ba2117887236f545f7b4711cbef896
CJ-The-Black-Child <congomigue@gmail.com>
2024-02-13 17:57:41 +0300

    Bug bounty hunting with Alx QA checker tools #3

---
+++ utils/basic.js
@@ -0,0 +1,12 @@
+const { ObjectId } = require('mongodb');
+
+const basicUtils = {
+  isValidId (id) {
+    if (!ObjectId.isValid(id)) {
+      return false;
+    }
+    return true;
+  }
+};
+
+module.exports = basicUtils;
---
+++ utils/user.js
@@ -0,0 +1,26 @@
+const redisClient = require('./redis');
+const dbClient = require('./db');
+
+const userUtils = {
+  async getUserIdAndKey (request) {
+    const obj = { userId: null, key: null };
+
+    const xToken = request.header('X-Token');
+
+    if (!xToken) return obj;
+
+    obj.key = `auth_${xToken}`;
+
+    obj.userId = await redisClient.get(obj.key);
+
+    return obj;
+  },
+
+  async getUser (query) {
+    const usersCollection = await dbClient.usersCollection();
+    const user = await usersCollection.findOne(query);
+    return user;
+  }
+};
+
+module.exports = userUtils;
