44d61772f11f976f8b3daeed9810022153dd90ce
CJ-The-Black-Child <congomigue@gmail.com>
2024-02-14 07:47:21 +0300

    Bug bounty hunting with Alx QA checker tools #9 - index.js fix

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const { getStatus, getStats } = require('../controllers/AppController');
-const { postNew, getMe } = require('../controllers/UsersController');
-const { getConnect, getDisconnect } = require('../controllers/AuthController');
-const { postUpload, getShow, getIndex, putPublish, putUnpublish, getFile } = require('../controllers/FilesController');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 function routingControl(app) {
   const router = express.Router();
   app.use('/', router);
 
   // App Controller
-  router.get('/status', getStatus);
-  router.get('/stats', getStats);
+  router.get('/status', AppController.getStatus);
+  router.get('/stats', AppController.getStats);
 
   // User Controller
-  router.post('/users', postNew);
+  router.post('/users', UsersController.postNew);
 
   // Authenticate user
-  router.get('/connect', getConnect);
-  router.get('/disconnect', getDisconnect);
-  router.get('/users/me', getMe);
+  router.get('/connect', AuthController.getConnect);
+  router.get('/disconnect', AuthController.getDisconnect);
+  router.get('/users/me', UsersController.getMe);
 
   // Files Controller
-  router.post('/files', postUpload);
-  router.get('/files/:id', getShow);
-  router.get('/files', getIndex);
-  router.put('/files/:id/publish', putPublish);
-  router.put('/files/:id/unpublish', putUnpublish);
-  router.get('/files/:id/data', getFile);
+  router.post('/files', FilesController.postUpload);
+  router.get('/files/:id', FilesController.getShow);
+  router.get('/files', FilesController.getIndex);
+  router.put('/files/:id/publish', FilesController.putPublish);
+  router.put('/files/:id/unpublish', FilesController.putUnpublish);
+  router.get('/files/:id/data', FilesController.getFile);
 }
 
 module.exports = routingControl;
