19f4bded027c455bac4e2d228c4bb404df01de90
CJ-The-Black-Child <congomigue@gmail.com>
2024-02-14 15:28:07 +0300

    Rolled back controllers/AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,41 +1,31 @@
 const sha1 = require('sha1');
 const { v4: uuidv4 } = require('uuid');
-const redisClient = require('../utils/redis');
-const User = require('../utils/user');
+const { set } = require('../utils/redis');
+const { getUser, getUserIdAndKey } = require('../utils/user');
 
 class AuthController {
   static async getConnect(request, response) {
     const Authorization = request.header('Authorization') || '';
-
     const credentials = Authorization.split(' ')[1];
 
-    if (!credentials) { return response.status(401).send({ error: 'Unauthorized' }); }
+    if (!credentials) return response.status(401).send({ error: 'Unauthorized' });
 
-    const decodedCredentials = Buffer.from(credentials, 'base64').toString('utf-8');
-    const [email, password] = decodedCredentials.split(':');
+    const [email, password] = Buffer.from(credentials, 'base64').toString('utf-8').split(':');
 
-    if (!email || !password) { return response.status(401).send({ error: 'Unauthorized' }); }
+    if (!email || !password) return response.status(401).send({ error: 'Unauthorized' });
 
-    const sha1Password = sha1(password);
-
-    const user = await User.getUser({
-      email,
-      password: sha1Password
-    });
+    const user = await getUser({ email, password: sha1(password) });
 
     if (!user) return response.status(401).send({ error: 'Unauthorized' });
 
     const token = uuidv4();
-    const key = `auth_${token}`;
-    const hoursForExpiration = 24;
-
-    await redisClient.set(key, user._id.toString(), hoursForExpiration * 3600);
+    await set(`auth_${token}`, user._id.toString(), 24 * 3600);
 
     return response.status(200).send({ token });
   }
 
   static async getDisconnect(request, response) {
-    const { userId, key } = await User.getUserIdAndKey(request);
+    const { userId, key } = await getUserIdAndKey(request);
 
     if (!userId) return response.status(401).send({ error: 'Unauthorized' });
 
