bb5babc9f1c19486f3db152860b8a16a54f81b1f
CJ-The-Black-Child <congomigue@gmail.com>
2024-02-14 07:34:31 +0300

    Bug bounty hunting with Alx QA checker tools #8 - semistandard syntax bug

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,11 +2,11 @@ const RedisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
 class AppController {
-  static getStatus(req, res) {
+  static getStatus (req, res) {
     res.status(200).send({ redis: RedisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  static async getStats(req, res) {
+  static async getStats (req, res) {
     res.status(200).send({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
   }
 }
